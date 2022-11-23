fb46f2b71b66084bbd459f450d087df77596f7a3
mvq7ww <mvq7ww@SZA0030898>
2022-11-23 17:17:04 +0100

    kirajzolja a háromszöget

---
+++ gyak9_mvq7ww/wwwroot/jsgyak.js
@@ -0,0 +1,37 @@
+﻿var faktoriálisR = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * faktoriálisR(n - 1)
+    }
+}
+//function számitás() {
+
+
+
+//}
+window.onload = () => {
+    for (var sor = 0; sor < 10; sor++) {
+
+        var újdiv = document.createElement("div")
+        újdiv.classList.add("sor")
+        document.getElementById("Pascal").appendChild(újdiv)
+
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+
+            var másikújdiv = document.createElement("div")
+            másikújdiv.classList.add("elem")
+            másikújdiv.innerHTML = `${sor}:${oszlop}`
+            újdiv.appendChild(másikújdiv)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
